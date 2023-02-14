6e62b33ba5813e3314a0bcf59be96911c84c075a
wabisabicode <tretyakov@uni-mainz.de>
2023-02-14 13:36:40 +0100

    try adding second stock; handle inventory exception

--- yahooquery_test.py
+++ yahooquery_test.py
@@ -3,32 +3,38 @@ from yahooquery import Ticker
 
 norm = 1000000
 
-stock = Ticker('mo')
+for stockname in ['mo', 'amt']:
+	stock = Ticker(stockname)
 
-print(stock.summary_detail)
+#	print(stock.summary_detail)
 
-# get yearly-relevant data: Inventory and TotalRevenue
-types_yearly = ['Inventory', 'TotalRevenue']
+	# get yearly-relevant data: Inventory and TotalRevenue
+	types_yearly = ['Inventory', 'TotalRevenue']
 
-yearly_info = stock.get_financial_data(types_yearly, trailing = False).tail(2)
+	yearly_info = stock.get_financial_data(types_yearly, trailing = False).tail(2)
 
-rev_last_y, rev_now = yearly_info['TotalRevenue']
-inv_last_y, inv_now = yearly_info['Inventory']
+	rev_last_y, rev_now = yearly_info['TotalRevenue']
 
-# get TTM-relevant data
+	while True: 
+		try:
+			inv_last_y, inv_now = yearly_info['Inventory']
+			break
+		except KeyError:
+			inv_last_y = 0
+			inv_now = 0
+			break
 
-types = ['EBITDA', 'CashAndCashEquivalents', 'TotalLiabilitiesNetMinorityInterest', 'CommonStockEquity', 'TotalDebt']
+	# get TTM-relevant data
 
-quartal_info = stock.get_financial_data(types, frequency='q', trailing=True)
-print (quartal_info)
-print ('')
+	types = ['EBITDA', 'CashAndCashEquivalents', 'TotalLiabilitiesNetMinorityInterest', 'CommonStockEquity', 'TotalDebt']
 
-ebitda = quartal_info.tail(1)['EBITDA'][0]
-cash = quartal_info['CashAndCashEquivalents'].iloc[-2]
-liability = quartal_info['TotalLiabilitiesNetMinorityInterest'].iloc[-2] 
-equity = quartal_info['CommonStockEquity'].iloc[-2]
-totalDebt = quartal_info['TotalDebt'].iloc[-2]
-asOfDate = quartal_info['asOfDate'].iloc[-2]
+	quartal_info = stock.get_financial_data(types, frequency='q', trailing=True)
 
-print (rev_last_y/norm, rev_now/norm, ebitda/norm, cash/norm, inv_last_y/norm, inv_now/norm, liability/norm, equity/norm, totalDebt/norm, asOfDate.strftime('%m/%y'), sep=' \t ')
-print (rev_last_y/norm, rev_now/norm, ebitda/norm, cash/norm, inv_last_y/norm, inv_now/norm, liability/norm, equity/norm, totalDebt/norm, asOfDate.strftime('%m/%y'), sep=' \t ')
+	ebitda = quartal_info.tail(1)['EBITDA'][0]
+	cash = quartal_info['CashAndCashEquivalents'].iloc[-2]
+	liability = quartal_info['TotalLiabilitiesNetMinorityInterest'].iloc[-2] 
+	equity = quartal_info['CommonStockEquity'].iloc[-2]
+	totalDebt = quartal_info['TotalDebt'].iloc[-2]
+	asOfDate = quartal_info['asOfDate'].iloc[-2]
+
+	print (rev_last_y/norm, rev_now/norm, ebitda/norm, cash/norm, inv_last_y/norm, inv_now/norm, liability/norm, equity/norm, totalDebt/norm, asOfDate.strftime('%m/%y'), sep=' \t ')
