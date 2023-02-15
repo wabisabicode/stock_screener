3bfca7110b74a0c0f0f4db0911b152cbb6f9965c
wabisabicode <tretyakov@uni-mainz.de>
2023-02-15 18:18:24 +0100

    refactor inventary to revenue calculation

--- yahooquery_test.py
+++ yahooquery_test.py
@@ -11,20 +11,20 @@ for stockname in ['abbv', 'alv.de', 'mo', 'amt']:#, 'googl']:
 #    print(stock.summary_detail)
 
     # get yearly-relevant data: Inventory and TotalRevenue
-    types_yearly = ['Inventory', 'TotalRevenue']
-
-    yearly_info = stock.get_financial_data(types_yearly, trailing = True).tail(2)
-    rev_last_y, rev_now = yearly_info['TotalRevenue']
-#    print(yearly_info)
-
-    while True: 
-        try:
-            inv_last_y, inv_now = yearly_info['Inventory']
-            break
-        except KeyError:
-            inv_last_y = 0
-            inv_now = 0
-            break
+#    types_yearly = ['Inventory', 'TotalRevenue']
+#
+#    yearly_info = stock.get_financial_data(types_yearly, trailing = True).tail(2)
+#    rev_last_y, rev_now = yearly_info['TotalRevenue']
+##    print(yearly_info)
+#
+#    while True: 
+#        try:
+#            inv_last_y, inv_now = yearly_info['Inventory']
+#            break
+#        except KeyError:
+#            inv_last_y = 0
+#            inv_now = 0
+#            break
 
     # get asset profile
     ebitda = stock.financial_data[stockname]['ebitda']
@@ -35,20 +35,20 @@ for stockname in ['abbv', 'alv.de', 'mo', 'amt']:#, 'googl']:
     
     rev = tosum_info.tail(4)['TotalRevenue']
     tot_rev = np.sum(rev)
+    rev_mrq = rev[-1]
 
-    av_inv = 0.
-    av_InvToRev = 0.
-    invToRev_now = 0.
+    # calculate inventory as % of last quarter revenue and ttm average thereof
+    av_inv_to_rev = 0.
+    inv_to_rev_mrq = 0.
     while True:
         try:
-            inv = tosum_info.tail(4)['Inventory'].copy()
-            if math.isnan(inv[-1]): inv[-1] = inv[-2]
-            inv_now = inv[-1]
+            inv = tosum_info.tail(4)['Inventory'].copy() # unpack inventory for 4 last quarters
+            inv_mrq = inv[-1]
+            if math.isnan(inv_mrq): inv_mrq = inv[-1] = inv[-2]
        
-            for i in [0, 1, 2, 3]: av_inv += inv[i]/rev[i]
-            av_InvToRev = av_inv / 4
-            print(inv[-1], rev[-1])
-            invToRev_now = inv[-1]/rev[-1]
+            for i in [0, 1, 2, 3]: av_inv_to_rev += inv[i] / rev[i]
+            av_inv_to_rev = av_inv_to_rev / 4
+            inv_to_rev_mrq = inv_mrq / rev_mrq
             break
         except KeyError:
             break
@@ -80,8 +80,8 @@ for stockname in ['abbv', 'alv.de', 'mo', 'amt']:#, 'googl']:
     totalDebt = quartal_info['TotalDebt'].iloc[-1]
     asOfDate = quartal_info['asOfDate'].iloc[-1]
 
-    print (rev_last_y/norm, tot_rev/norm, ebitda/norm, cash/norm, 
-            "{:,.2f}%".format(av_InvToRev*100), "{:,.2f}%".format(invToRev_now*100), 
+    print (tot_rev/norm, ebitda/norm, cash/norm, 
+            "{:,.2f}%".format(av_inv_to_rev*100), "{:,.2f}%".format(inv_to_rev_mrq*100), 
             liability/norm, equity/norm, totalDebt/norm, asOfDate.strftime('%m/%y'), sep=' \t ')
 
 #    interest = quartal_info['InterestExpense']
