126d5a05eced3c354adea144940360f8b9f10771
wabisabicode <tretyakov@uni-mainz.de>
2023-02-16 23:31:22 +0100

    handle no quarterly revenue situation

--- yahooquery_test.py
+++ yahooquery_test.py
@@ -4,42 +4,11 @@ import numpy as np
 import math
 
 def main():
-    for stockname in ['abbv', 'alv.de', 'mo', 'amt', 'amgn', 'ay', 'bti', 'blk', 'bepc', 'csco', 'dlr', 'enb', 'ncv.de', 'hei.de', 'ibe.mc']:#, 'googl']:
+    for stockname in ['abbv', 'alv.de', 'mo', 'amt', 'amgn', 'ay', 'bti', 'blk', 'bepc', 'csco', 'dlr', 'enb', 'ecv.de', 'hei.de', 'ibe.mc']:#, 'googl']:
+
         stock = Ticker(stockname)
 
-        # revenue has to be summed up
-        types_tosum = ['TotalRevenue','Inventory']
-        tosum_info = stock.get_financial_data(types_tosum, frequency='q', trailing=False)
-
-        print (tosum_info)
-
-        while True:
-            try: 
-                rev = tosum_info.tail(4)['TotalRevenue']
-                break
-            except KeyError:
-                print ("Empty array")
-                break
-
-        rev = tosum_info.tail(4)['TotalRevenue']
-        tot_rev = np.sum(rev)
-        rev_mrq = rev[-1]
-
-        # calculate inventory as % of last quarter revenue and ttm average thereof
-        av_inv_to_rev = 0.
-        inv_to_rev_mrq = 0.
-        while True:
-            try:
-                inv = tosum_info.tail(4)['Inventory'].copy() # unpack inventory for 4 last quarters
-                inv_mrq = inv[-1]
-                if math.isnan(inv_mrq): inv_mrq = inv[-1] = inv[-2]
-           
-                for i in [0, 1, 2, 3]: av_inv_to_rev += inv[i] / rev[i]
-                av_inv_to_rev = av_inv_to_rev / 4
-                inv_to_rev_mrq = inv_mrq / rev_mrq
-                break
-            except KeyError:
-                break
+        av_inv_to_rev, inv_to_rev_mrq = calc_revenue_inventory_stats(stock)
 
         ebitda = get_ttm_ebitda(stock, stockname)
        
@@ -90,6 +59,48 @@ def get_mrq_financial_strength(_stock):
     _net_debt = totalDebt - cash
 
     return _equity_ratio, _net_debt, _asOfDate
+
+
+def calc_revenue_inventory_stats(_stock):
+    # revenue has to be summed up
+    types_tosum = ['TotalRevenue','Inventory']
+    tosum_info = _stock.get_financial_data(types_tosum, frequency='q', trailing=False)
+
+    print (tosum_info)
+
+    tot_rev = 0
+    rev_mrq = 0
+    no_rev_data = False
+
+    while True:
+        try: 
+            rev = tosum_info.tail(4)['TotalRevenue']
+            tot_rev = np.sum(rev)
+            rev_mrq = rev[-1]
+            break
+        except KeyError:
+            no_rev_data = True
+            print ("Empty array")
+            break
+
+    # calculate inventory as % of last quarter revenue and ttm average thereof
+    _av_inv_to_rev = 0.
+    _inv_to_rev_mrq = 0.
+
+    while True and no_rev_data == False:
+        try:
+            inv = tosum_info.tail(4)['Inventory'].copy() # unpack inventory for 4 last quarters
+            inv_mrq = inv[-1]
+            if math.isnan(inv_mrq): inv_mrq = inv[-1] = inv[-2]
+       
+            for i in [0, 1, 2, 3]: _av_inv_to_rev += inv[i] / rev[i]
+            _av_inv_to_rev = _av_inv_to_rev / 4
+            _inv_to_rev_mrq = inv_mrq / rev_mrq
+            break
+        except KeyError:
+            break
+
+    return _av_inv_to_rev, _inv_to_rev_mrq
 
 if __name__ == "__main__":
     main()
