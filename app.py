20143e249c29ccf518020fdc53c529700358ba1b
wabisabicode <tretyakov@uni-mainz.de>
2024-03-26 14:23:06 +0100

    clean up debugging info

--- app.py
+++ app.py
@@ -79,7 +79,6 @@ def results():
                 'ev_to_rev_ui': ev_to_rev,
                 'p_to_ocf_ui': p_to_ocf
             })
-            print(data)
         else:
             data.append({})
 
