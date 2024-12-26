a704f267ff589c2abca2aa9e0713a6d926d98f7a
wabisabicode <tretyakov@uni-mainz.de>
2024-12-26 13:14:35 +0100

    add daikin to watchlist

--- yahooquery_cli.py
+++ yahooquery_cli.py
@@ -102,7 +102,7 @@ def form_stock_list(_listarg):
 
         "watch": [
             'tte', 'shel', '', '', 'apd', 'hei.de', 'lin', 'bas.de', '', '',
-            'mmm', 'dhl.de', 'fra.de', 'ge', 'hot.de', 'lmt', 'raa.de', '', '',
+            'mmm', '6367.t', 'dhl.de', 'fra.de', 'ge', 'hot.de', 'lmt', 'raa.de', '', '',
             'mcd', 'ads.de', 'prx.as', 'sbux', 'vfc', '', '',
             '2502.t', 'ko', 'k', 'nesn.sw', 'pep', 'pm', 'swa.de', 'ul', '', '',
             'amgn', 'bayn.de', 'bion.sw', 'bmy', 'cvs', 'gild',
