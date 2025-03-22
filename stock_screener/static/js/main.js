f09492b6ae0a85045dc1952aac81ec7f131dc5b9
wabisabicode <tretyakov@uni-mainz.de>
2025-03-22 18:43:39 +0100

    remove superfluous static files

--- stock_screener/static/js/main.js
+++
@@ -1,8 +0,0 @@
-const socket = io();
-
-socket.on('new_result', function(data) {
-    const resultsContainer = document.getElementById('results-container');
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `<td>${data.stock}</td><td>${data.data}</td>`;
-    resultsContainer.appendChild(newRow);
-});
