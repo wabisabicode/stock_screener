1ceed7e0c55e3cc23fbce976f80c3f2e70283d17
wabisabicode <tretyakov@uni-mainz.de>
2025-04-06 23:33:57 +0200

    fix imports of data functions

--- stock_screener/crud.py
+++ stock_screener/crud.py
@@ -1,17 +1,17 @@
 from datetime import datetime
+
 from yahooquery import Ticker
 
-from .yahooquery_cli import (calc_rev_inv_stats, get_ann_gp_margin,
-                             get_ev_to_rev, get_mrq_fin_strength,
-                             get_mrq_gp_margin, get_p_to_ocf, get_q_rev_growth,
-                             get_ttm_ebitda_ocf, get_yearly_revenue)
-from .utils import elapsed_time
+from .utils import (calc_rev_inv_stats, elapsed_time, get_ann_gp_margin,
+                    get_ev_to_rev, get_mrq_fin_strength, get_mrq_gp_margin,
+                    get_p_to_ocf, get_q_rev_growth, get_ttm_ebitda_ocf,
+                    get_yearly_revenue)
 
 
 def update_stock_data(stockname):
     time_start_anal = datetime.now()
 
-    stock = Ticker(stockname)
+    stock = Ticker(stockname, asynchronous=False)
     time_got_ticker = elapsed_time(time_start_anal, 'Got ticker in')
     fin_data = stock.financial_data[stockname]
     time_got_fin_data = elapsed_time(time_got_ticker, 'Got fin_data in')
@@ -42,7 +42,7 @@ def update_stock_data(stockname):
         key_stats = stock.key_stats[stockname]
     else:
         key_stats = 0
-    
+
     ev_to_rev = get_ev_to_rev(stock, key_stats)
 
     summary_detail = stock.summary_detail[stockname]
@@ -56,22 +56,21 @@ def update_stock_data(stockname):
     stock_data = {
         'symbol': stockname,
         'equity_ratio': equity_ratio * 100,
-        'ndebt_to_ebitda': equity_ratio * 100,
-        'net_debt_ebitda': net_debt / ebitda,
+        'net_debt_to_ebitda': net_debt / ebitda,
         'inv_to_rev_mrq': inv_to_rev_mrq * 100,
-        'avg_inv_to_rev': avg_inv_to_rev * 100,
-        'q_rev_growth_ui': q_rev_growth * 100,
-        'av_rev_growth_ui': av_rev_growth * 100 - 100,
-        'mrq_gp_margin_ui': mrq_gp_margin * 100, 
-        'av_gp_margin_ui': av_gp_margin * 100,
-        'mrq_ocf_margin_ui': mrq_ocf_margin * 100, 
-        'av_ocf_margin_ui': av_ocf_margin * 100,
-        'mrq_fcf_margin_ui': mrq_fcf_margin * 100, 
-        'av_fcf_margin_ui': av_fcf_margin * 100,
+        'av_inv_to_rev': avg_inv_to_rev * 100,
+        'q_rev_growth': q_rev_growth * 100,
+        'av_rev_growth': av_rev_growth * 100 - 100,
+        'mrq_gp_margin': mrq_gp_margin * 100,
+        'av_gp_margin': av_gp_margin * 100,
+        'mrq_ocf_margin': mrq_ocf_margin * 100,
+        'av_ocf_margin': av_ocf_margin * 100,
+        'mrq_fcf_margin': mrq_fcf_margin * 100,
+        'av_fcf_margin': av_fcf_margin * 100,
         'as_of_date': asOfDate.strftime('%m/%y'),
-        'remarks_ui': remarks,
-        'ev_to_rev_ui': ev_to_rev,
-        'p_to_ocf_ui': p_to_ocf
+        'remarks': remarks,
+        'ev_to_rev': ev_to_rev,
+        'p_to_ocf': p_to_ocf
     }
 
     return stock_data
