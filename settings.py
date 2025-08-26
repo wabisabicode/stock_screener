2816ebd22866d1977ddaddb2be324fb9583254f3
wabisabicode <tretyakov@uni-mainz.de>
2025-08-26 12:30:52 +0200

    set default value of SQLALCHEMY_DATABASE_URI

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite://')
     SECRET_KEY = os.getenv('SECRET_KEY')
