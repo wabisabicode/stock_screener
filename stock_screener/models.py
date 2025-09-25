6bffe40edefdd542e6dbe065fd5d41df01a91cf3
wabisabicode <tretyakov@uni-mainz.de>
2025-09-25 15:38:09 +0200

    update fields of the Stock model

--- stock_screener/models.py
+++ stock_screener/models.py
@@ -9,32 +9,43 @@ class ReportType(enum.Enum):
     ANNUAL = 'ANNUAL'
 
 
-class StockData(db.Model):
+class Stock(db.Model):
+    # Never change:
     id = db.Column(db.Integer, primary_key=True)  # Unique identifier
-    ticker = db.Column(db.String(10), nullable=False)  # Stock ticker like 'O'
+    ticker = db.Column(db.String(10), nullable=False,
+                       unique=True, index=True)  # Stock ticker like 'O'
 
-    equity_ratio = db.Column(db.Float)  # Equity ratio as a percentage
-    ndebt_to_ebitda = db.Column(db.Float)  # Debt to EBITDA ratio
-    net_debt_ebitda = db.Column(db.Float)  # Net debt to EBITDA ratio
+    reports = db.relationship(
+        'FinancialReport', backref='stock',
+        lazy='dynamic', cascade="all, delete-orphan")
 
-    inv_to_rev_mrq = db.Column(db.Float)  # Inventory to revenue (mrq)
+    # Calculated once per quarter from the new FinancialReport
+    curr_equity_ratio = db.Column(db.Float)  # Equity ratio as a percentage
+    curr_net_debt_to_ebitda = db.Column(db.Float)  # Net Debt to EBITDA ratio
+
+    mrq_inv_to_rev = db.Column(db.Float)  # Inventory to revenue (mrq)
     avg_inv_to_rev = db.Column(db.Float)  # Average inventory to revenue
 
-    q_rev_growth = db.Column(db.Float)  # Quarterly revenue growth
-    av_rev_growth = db.Column(db.Float)  # Annual revenue growth
+    mrq_yoy_rev_growth = db.Column(db.Float)  # Quarterly revenue growth
+    avg_rev_growth = db.Column(db.Float)  # Annual revenue growth
     mrq_gp_margin = db.Column(db.Float)  # Gross profit margin (mrq)
-    av_gp_margin = db.Column(db.Float)  # Average gross profit margin
-
-    mrq_ocf_margin = db.Column(db.Float)  # Operating cash flow margin (mrq)
-    av_ocf_margin = db.Column(db.Float)  # Average operating cash flow margin
+    avg_gp_margin = db.Column(db.Float)  # Average gross profit margin
     mrq_fcf_margin = db.Column(db.Float)  # Free cash flow margin (mrq)
-    av_fcf_margin = db.Column(db.Float)  # Average free cash flow margin
+    avg_fcf_margin = db.Column(db.Float)  # Average free cash flow margin
 
-    as_of_date = db.Column(db.String(5))  # Date in 'MM/YY' format
+    curr_div_rate_rev = db.Column(db.Float)  # Current dividend in (Eur, $)
+
+    curr_end_date = db.Column(db.Date)  # Date in 'MM/YY' format
     remarks = db.Column(db.String(50))  # Additional remarks
 
-    ev_to_rev = db.Column(db.Float)  # EV to revenue
-    p_to_ocf = db.Column(db.Float)  # Price to operating cash flow
+    # Calculated daily:
+    curr_ev_to_rev = db.Column(db.Float)  # EV to revenue
+    avg_ev_to_rev = db.Column(db.Float)  # 5Y average EV to revenue
+    curr_fcf_to_rev = db.Column(db.Float)  # FCF to revenue
+    avg_fcf_to_rev = db.Column(db.Float)  # 5Y average FCF to revenue
+
+    curr_fwd_div = db.Column(db.Float)  # current forward yield
+    avg_fwd_div = db.Column(db.Float)  # 5Y average forward yield
 
     @staticmethod
     def update_or_create(session, stock_dict):
@@ -44,7 +55,7 @@ class StockData(db.Model):
             raise ValueError(
                 'The stock dictionary must contain the "ticker" key.')
 
-        stock = session.query(StockData).filter_by(ticker=ticker).first()
+        stock = session.query(Stock).filter_by(ticker=ticker).first()
 
         if stock:
             for key, value in stock_dict.items():
@@ -56,7 +67,7 @@ class StockData(db.Model):
         else:
             print(
                 f'Stock with ticker {ticker} not found. Adding a new record.')
-            new_stock = StockData(**stock_dict)
+            new_stock = Stock(**stock_dict)
             session.add(new_stock)
             session.commit()
             print(f'New stock {ticker} added successfully.')
