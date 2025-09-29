bb8f774e4baaa7df4dc5d10f4a34d1eb3e2149df
wabisabicode <tretyakov@uni-mainz.de>
2025-09-29 10:12:14 +0200

    add FLASK_APP and FLASK_DEBUG to settings.py

--- settings.py
+++ settings.py
@@ -2,5 +2,7 @@ import os
 
 
 class Config(object):
+    FLASK_APP = os.getenv('FLASK_APP', 'stock_screener')
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG', '1')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite://')
     SECRET_KEY = os.getenv('SECRET_KEY')
